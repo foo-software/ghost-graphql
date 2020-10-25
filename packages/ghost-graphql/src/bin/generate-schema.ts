77505621dc342a2edbbf58e2930ff9ae2e017385
Adam Henson <adamhenson1979@gmail.com>
2020-10-25 01:56:57 -0400

    feat: generate schema

---
+++ packages/ghost-graphql/src/bin/generate-schema.ts
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+import fs from 'fs';
+import { printSchema } from 'graphql';
+import path from 'path';
+import schema from '../schema';
+
+fs.writeFileSync(
+  path.resolve(__dirname, '../../schema.graphql'),
+  printSchema(schema)
+);
+
+console.log('Schema generated ✔');
