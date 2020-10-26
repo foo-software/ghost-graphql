f4de4888bd47a787a06954be939e108b0c23893c
Foo <notifications@foo.software>
2020-10-25 21:17:33 -0400

    fix: commit schema

--- packages/ci-tools/src/bin/commit-schema.ts
+++ packages/ci-tools/src/bin/commit-schema.ts
@@ -3,8 +3,6 @@ import * as git from '../git';
 
 const run = async () => {
   try {
-    console.log('⌛ configuring git...');
-    git.config();
     console.log('⌛ git commit...');
     git.add();
     git.commit('chore: generate schema');
