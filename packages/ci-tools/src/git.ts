82555c1bd6d1183f16aa1517feeffa699b808129
Adam Henson <adamhenson1979@gmail.com>
2020-10-25 21:02:22 -0400

    Revert "chore: sync packages"
    This reverts commit c033ef1b3245b5e713621123f7533e7dac1794a0.

--- packages/ci-tools/src/git.ts
+++ packages/ci-tools/src/git.ts
@@ -1,27 +1,26 @@
-import Git from 'git-commands';
+import shell from './shell';
 
 const { PERSONAL_ACCESS_TOKEN: TOKEN } = process.env;
 
 const GIT_URL = `https://foo-software-bot:${TOKEN}@github.com/foo-software/ghost-graphql`;
-const git = new Git({ reps: GIT_URL });
 
 export const config = () => {
-  console.log(git.command('checkout master'));
-  console.log(
-    git.command('config --global user.email notifications@foo.software')
-  );
-  console.log(git.command('config --global user.name Foo Bot'));
+  shell('git config --global user.email notifications@foo.software');
+  shell('git config --global user.name Foo Bot');
 };
 
 export const checkout = (branch: string = 'master') => {
-  console.log(git.command('fetch'));
-  console.log(git.command(`checkout ${branch} && git pull`));
+  shell(`git remote set-url origin ${GIT_URL}`);
+  shell('git fetch');
+  shell(`git checkout ${branch} && git pull`);
 };
 
 export const add = () => {
-  console.log(git.command('add .'));
+  shell('git add .');
 };
 
 export const commit = (message: string) => {
-  console.log(git.command(`commit -am '${message} (skip ci)'`));
+  shell(`git remote set-url origin ${GIT_URL}`);
+  shell('git fetch');
+  shell(`git commit -am '${message} (skip ci)'`);
 };
