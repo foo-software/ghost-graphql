663cdba2142e8e9ebd7b00ec92735a9913d096fc
adamhenson <adamhenson1979@gmail.com>
2020-10-27 18:56:53 -0400

    feat: authors tests

---
+++ packages/ghost-graphql-integration-tests/src/mocks/authorsResponse.ts
@@ -0,0 +1,46 @@
+export default {
+  authors: [
+    {
+      bio: `I'm an American novelist, best known for my work writing about a brave jungle hero called Tarzan and his muse, Jane.`,
+      url: 'https://demo.ghost.io/author/edgar/',
+      website: null,
+      twitter: null,
+      cover_image:
+        'https://demo.ghost.io/content/images/2018/08/Screenshot-2018-08-23-14.08.38.png',
+      location: 'Chicago, Illinois',
+      slug: 'edgar',
+      id: '5979a779df093500228e958f',
+      facebook: null,
+      meta_description: null,
+      name: 'Edgar Rice Burroughs',
+      meta_title: null,
+      profile_image: 'https://demo.ghost.io/content/images/2018/10/edgar.jpg',
+    },
+    {
+      bio: 'The professional publishing platform',
+      url: 'https://demo.ghost.io/author/ghost/',
+      twitter: '@tryghost',
+      website: 'https://ghost.org',
+      slug: 'ghost',
+      id: '5951f5fca366002ebd5dbef7',
+      cover_image: null,
+      location: null,
+      meta_title: null,
+      profile_image:
+        '//www.gravatar.com/avatar/2bfa103a13c88b5ffd26da6f982f11df?s=250&d=mm&r=x',
+      facebook: 'ghost',
+      meta_description: null,
+      name: 'Ghost',
+    },
+  ],
+  meta: {
+    pagination: {
+      pages: 6,
+      total: 11,
+      page: 2,
+      next: 3,
+      prev: 1,
+      limit: 2,
+    },
+  },
+};
