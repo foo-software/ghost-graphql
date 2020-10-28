64990db83e526047379e1b93aef556e26179079c
adamhenson <adamhenson1979@gmail.com>
2020-10-27 23:27:23 -0400

    chore: lint

--- packages/ghost-graphql/src/types/GhostAuthor.ts
+++ packages/ghost-graphql/src/types/GhostAuthor.ts
@@ -1,6 +1,5 @@
 import { GraphQLNonNull, GraphQLObjectType, GraphQLString } from 'graphql';
 import createConnectionType from '../typeCreators/createConnectionType';
-import fs from 'fs';
 
 const GhostAuthor = new GraphQLObjectType({
   name: 'GhostAuthor',
