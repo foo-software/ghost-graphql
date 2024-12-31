1130cd0d5fd76bbf097f32d463cd256bdcea3be9
adamhenson <adamhenson1979@gmail.com>
2024-12-30 21:20:24 -0500

    feat: npm workspace publish fix

--- packages/ghost-graphql-ci-tools/src/npm.ts
+++ packages/ghost-graphql-ci-tools/src/npm.ts
@@ -2,7 +2,7 @@ import shell from './shell';
 
 export const publish = () => {
   shell(
-    `npm publish --workspace=@foo-software/ghost-graphql --workspace=@foo-software/ghost-graphql-server `
+    `npm publish --workspace=@foo-software/ghost-graphql --workspace=@foo-software/ghost-graphql-server`
   );
 };
 
